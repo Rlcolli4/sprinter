203cd0b3bcb575e9f7a6e6042e9e6438e98b8213
Robert Collins Jr. <robbie@sitesusa.local>
2024-02-21 18:50:32 -0700

    Added some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  // TODO: Add Icons for the route
+  links: { id: number, title: string, route: string }[] = [
+    { id: 1, title: "Home", route: "home" },
+    { id: 2, title: "Teams", route: "teams" },
+    { id: 3, title: "Projects", route: "projects" },
+  ];
+
+  constructor(private router: Router) { }
+
+  onNavItemClick(link: { id: number, title: string, route: string }): void {
+    console.log(link);
+    this.router.navigateByUrl(link.route);
+  }
+}
