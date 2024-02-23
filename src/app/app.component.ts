614939d5088cd77eb3389593f1381e4f8451c1d6
Robert Collins Jr. <robbie@sitesusa.local>
2024-02-23 14:49:41 -0700

    Fixed up some css and settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { LinkInfo } from './models/navigation/link-info.model';
 
@@ -7,8 +7,13 @@ import { LinkInfo } from './models/navigation/link-info.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  // TODO: Add Icons for the route
+export class AppComponent implements AfterViewInit {
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.updateMobileFlag();
+  }
+
+  isMobileSize: boolean = false;
   links: LinkInfo[] = [
     { id: 1, title: "Home", route: "home", icon: "home" },
     { id: 2, title: "Teams", route: "teams", icon: "people" },
@@ -16,5 +21,19 @@ export class AppComponent {
   ];
 
   constructor(public router: Router) { }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.updateMobileFlag();
+    }, 250);
+  }
+
+  private updateMobileFlag(): void {
+    if (window.innerWidth >= 1024) {
+      this.isMobileSize = false;
+    } else {
+      this.isMobileSize = true;
+    }
+  }
 
 }
